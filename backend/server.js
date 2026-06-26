40d96ef2bee243531ae4bfc10b2b85b10aea4ba9
karthik245322748042-art <karthik245322748042@gmail.com>
2026-06-26 17:13:14 +0530

    Fix CORS origin issue

--- backend/server.js
+++ backend/server.js
@@ -44,13 +44,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
   : ["http://localhost:5173", "http://localhost:3000", "http://localhost:5000"];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: true, // Dynamically reflect origin to allow all
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Active-User-Id"]
